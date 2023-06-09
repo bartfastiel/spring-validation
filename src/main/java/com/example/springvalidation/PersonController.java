6b84b74a3481f3d23810e86187929875166ce45d
Daniel Schwarz <bartfastiel@users.noreply.github.com>
2023-06-09 10:44:41 +0200

    Add code of live-coding session

--- src/main/java/com/example/springvalidation/PersonController.java
+++ src/main/java/com/example/springvalidation/PersonController.java
@@ -1,10 +1,10 @@
 package com.example.springvalidation;
 
 import jakarta.validation.Valid;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.websocket.server.PathParam;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindException;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/persons")
@@ -12,6 +12,15 @@ public class PersonController {
 
     @PutMapping
     Person putPerson(@RequestBody @Valid Person person) {
+        if (person.firstname().equalsIgnoreCase("Pumuckl")) {
+            throw new UnwantedUserException();
+        }
         return person;
+    }
+
+    @ExceptionHandler(UnwantedUserException.class)
+    @ResponseStatus(HttpStatus.FORBIDDEN)
+    ErrorMessage handleUnwantedUserException() {
+        return new ErrorMessage("Leider nicht zugelassen");
     }
 }
---
+++ src/main/java/com/example/springvalidation/UnwantedUserException.java
@@ -0,0 +1,4 @@
+package com.example.springvalidation;
+
+public class UnwantedUserException extends RuntimeException {
+}
